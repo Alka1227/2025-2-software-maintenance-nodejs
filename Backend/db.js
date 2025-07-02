0ee2ba7d3c143d4bb56351c1fbefb011b172a2dc
Javier Karim Reyes García <javier_karim@outlook.com>
2025-07-02 08:19:06 -0600

    Fixed #13 migracion BD
    Migracion de BD de SQL a postgres

--- Backend/db.js
+++ Backend/db.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
+  'task_App', 
+  'postgres', 
+  '12345678', 
   {
-    host: process.env.DB_HOST, 
-    port: process.env.DB_PORT, 
-    dialect: 'mysql',
+    host: 'localhost', 
+    port: '5432', 
+    dialect: 'postgres',
   }
 );
 
